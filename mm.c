64f05ff4340362618c3a15e970c646290cd828be
Sam Arrants <arrants8@gmail.com>
2018-06-07 14:13:27 -0500

    original plus get_prev/next

--- mm.c
+++ mm.c
@@ -60,6 +60,14 @@ team_t team = {
 #define GET_SIZE(p) (GET(p) & ~0x7)
 #define GET_ALLOC(p) (GET(p) & 0x1)
 
+
+/* Read the previous and next of the node */
+#define GET_PREV(p) ((unsigned int *)GET(p) + 1) //this should be the address of prev node
+#define GET_NEXT(p) ((unsigned int *)GET(p) + 2) //this should be the address of next node
+
+
+
+
 /* Given block pointer bp, find address of its header and footer */
 #define HDRP(bp) ((char *)(bp) - WSIZE)
 #define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
@@ -202,7 +210,7 @@ void *mm_malloc(size_t size)
     //search the free list for a fit
     
     //for explicit list, we want to jump from prologue -> free block -> free block
-    
+    /* Sam sudo code
     currnode = prologue.next
      if currnode.size < asize {
         currnode = currnode.next}
@@ -211,14 +219,14 @@ void *mm_malloc(size_t size)
          // currnode.prev.next = currnode.next
          // currnode.next.prev = currnode.prev
         return bp;
-     }
+     }*/
      
     
-    /*
+    
     if ((bp = find_fit(asize)) != NULL){
         place(bp, asize);
         return bp;
-    }*/
+    }
     
     // No fit found. Get more memory and place the block
     extendsize = MAX(asize, CHUNKSIZE);
